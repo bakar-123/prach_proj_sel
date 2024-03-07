d54c4ab2bbe4b4dd6edd6ea8615a12047e5b1941
bakar-123 <113510293+bakar-123@users.noreply.github.com>
2024-03-07 08:23:24 +0530

    Update Gitday4.java
    forgot ; added

--- prach_project/src/main/java/forgitpackagepracticeday1/Gitday4.java
+++ prach_project/src/main/java/forgitpackagepracticeday1/Gitday4.java
@@ -2,6 +2,6 @@ package forgitpackagepracticeday1;
 
 public class Gitday4 {
 
-  String s = "this class is added from github for the reason to pull the code from github to selenium"
+  String s = "this class is added from github for the reason to pull the code from github to selenium";
 
 }
